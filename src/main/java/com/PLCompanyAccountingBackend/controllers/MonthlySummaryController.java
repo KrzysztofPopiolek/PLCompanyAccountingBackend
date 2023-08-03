de67c60f3c0294037901fcd4c171d57c8a761e02
Krzysztof Popiołek <kapopiolek@gmail.com>
2023-08-03 12:48:23 +0100

    Utworzenie list miesiecy i roku

--- src/main/java/com/PLCompanyAccountingBackend/controllers/MonthlySummaryController.java
+++ src/main/java/com/PLCompanyAccountingBackend/controllers/MonthlySummaryController.java
@@ -1,12 +1,17 @@
 package com.PLCompanyAccountingBackend.controllers;
 
+import com.PLCompanyAccountingBackend.exceptions.ResourceNotFoundException;
 import com.PLCompanyAccountingBackend.models.MonthlySummary;
 import com.PLCompanyAccountingBackend.repository.MonthlySummaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -16,9 +21,19 @@ public class MonthlySummaryController {
     @Autowired
     private MonthlySummaryRepository monthlySummaryRepository;
 
-    @GetMapping("/allMonthlySummary")
-    public List<MonthlySummary> getAllMonthlySummary() {
-        return monthlySummaryRepository.findAll();
+    @GetMapping("/allMonthlySummary/{date}")
+    public List<MonthlySummary> getAllMonthlySummary(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date) {
+        List<MonthlySummary> monthlySummaries = monthlySummaryRepository.findAll();
+        List<MonthlySummary> foundMonthlySummaries = new ArrayList<>();
+        for (int i = 0; i < monthlySummaries.size(); i++) {
+            if (monthlySummaries.get(i).getMonthYear().compareTo(date) == 0) {
+                for (int j = i; j < i + 12; j++) {
+                    foundMonthlySummaries.add(monthlySummaries.get(j));
+                }
+                return foundMonthlySummaries;
+            }
+        }
+        throw new ResourceNotFoundException("Date not found");
     }
 
 
